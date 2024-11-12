12b11eb0e71ea50cac6b82aaa27e0ac87584bdf0
zer0gear <deepsheepsleep@gmail.com>
2024-11-11 19:12:36 -0600

    Add zer0 to end of node names to avoid potential collisions

--- __init__.py
+++ __init__.py
@@ -2,8 +2,8 @@ from .multiline_string import MultilineStringNode
 from .list_concat_node import ListConcatNode
 
 NODE_CLASS_MAPPINGS = {
-    "List Concat Node": ListConcatNode,
-    "Multiline String Node": MultilineStringNode,
+    "List Concat Node (zer0)": ListConcatNode,
+    "Multiline String Node (zer0)": MultilineStringNode,
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS"]
